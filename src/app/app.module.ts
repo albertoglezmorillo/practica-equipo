279022849d6895c7a8e9d2f9ee8cea38f6a944d0
Alfonso <alfonsoderojasperez@gmail.com>
2022-04-26 11:28:56 +0200

    actualizacion de tarjeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetaClienteComponent } from './componentes/tarjeta-cliente/tarjeta-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaClienteComponent
   ],
   imports: [
     BrowserModule,
