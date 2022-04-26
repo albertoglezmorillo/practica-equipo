bd87844f977fbb8cca763b89765df958144bed25
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-04-26 12:18:40 +0200

    importados los componenetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TarjetaClienteComponent } from './componentes/tarjeta-cliente/tarjeta-cliente/tarjeta-cliente.component';
-
+import { TarjetaClienteComponent } from './components/tarjeta-cliente/tarjeta-cliente.component';
+import { BotonComponent } from './components/boton/boton.component';
+import { InputComponent } from './components/input/input.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TarjetaClienteComponent
+    TarjetaClienteComponent,
+    BotonComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
