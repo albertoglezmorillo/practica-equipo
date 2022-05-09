27e49fe933bbb327ba5496963791d2ae58e337a6
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-05-09 09:16:05 +0200

    console logs y metodos sobrantes

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -13,8 +13,8 @@ export class ClienteService{
   
   constructor(private http: HttpClient) {
     let anio = this.fecha.getFullYear();
-    let mes = this.fecha.getMonth() < 9 ? '0'+(this.fecha.getMonth()+1) : this.fecha.getMonth()+1;
-    let dia = this.fecha.getDate() < 9 ? '0'+this.fecha.getDate() : this.fecha.getDate();
+    let mes = this.fecha.getMonth() <= 9 ? '0'+(this.fecha.getMonth()+1) : this.fecha.getMonth()+1;
+    let dia = this.fecha.getDate() <= 9 ? '0'+this.fecha.getDate() : this.fecha.getDate();
     let mensaje = this.nombre+anio+mes+dia;
     var token = CryptoJS.SHA384(mensaje).toString();
 
