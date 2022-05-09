78f7879e56c1a2ae557c51cc1ff343c4e9c9b93e
Alfonso <alfonsoderojasperez@gmail.com>
2022-05-09 09:22:32 +0200

    Retoques

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -13,8 +13,8 @@ export class ClienteService{
   
   constructor(private http: HttpClient) {
     let anio = this.fecha.getFullYear();
-    let mes = this.fecha.getMonth() < 9 ? '0'+(this.fecha.getMonth()+1) : this.fecha.getMonth()+1;
-    let dia = this.fecha.getDate() < 9 ? '0'+this.fecha.getDate() : this.fecha.getDate();
+    let mes = this.fecha.getMonth() <= 9 ? '0'+(this.fecha.getMonth()+1) : this.fecha.getMonth()+1;
+    let dia = this.fecha.getDate() <= 9 ? '0'+this.fecha.getDate() : this.fecha.getDate();
     let mensaje = this.nombre+anio+mes+dia;
     var token = CryptoJS.SHA384(mensaje).toString();
 
@@ -22,6 +22,7 @@ export class ClienteService{
   }
 
   getCliente(parametros: any):Observable<any>{
+    
     return this.http.get<any>(this.url,{headers: this.cabecera, params: parametros});
   }
   deleteCliente(parametros: any) :Observable<any>{
