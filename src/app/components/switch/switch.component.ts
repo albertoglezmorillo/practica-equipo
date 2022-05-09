948b9365450251b394aecc620dc3ac531dd77881
Alberto Morillo <morillo@azurglobal.es>
2022-05-09 12:22:58 +0200

    Subida correccion

--- src/app/components/switch/switch.component.ts
+++ src/app/components/switch/switch.component.ts
@@ -6,16 +6,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./switch.component.scss']
 })
 export class SwitchComponent {
-  @Input() valores: string[] = ['No Activo', 'Activo', 'Estado'];
+  @Input() etiqueta: string = '';
+  @Input() enableVal: string = 'Activo';
+  @Input() disableVal: string = 'Inactivo';
+  @Input() neutroVal: string = '-';
+  // @Input() valores: string[] = ['No Activo', 'Activo', 'Estado'];
   @Input() numeroSwitch: number = 3;
   @Input() estilo: string = 'sm';
   @Output() changed = new EventEmitter<any>();
   @Input() valorInput: any = '';
-  
-  
+
+
 
   cambioInput(valor: string) {
-    
+
     this.valorInput=valor;
     this.changed.emit(valor);
   }
